2eb8c58c90418513320ea4803fbf0ba9d93d7683
Raazia-26 <razi.raazia@gmail.com>
2024-04-04 23:41:40 +0200

    added github repository URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather React App</div>;
+  return (
+    <div className="App">
+      Weather React App
+      <footer>
+        This project was coded by Raazia Razi and is{" "}
+        <a
+          href="https://github.com/Raazia-26/weather-react-App"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
