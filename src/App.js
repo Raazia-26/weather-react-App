fee01bf22babe23440926334ae55d8c2d7bacf03
Raazia-26 <razi.raazia@gmail.com>
2024-04-05 00:08:48 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather React App
-      <footer>
-        This project was coded by Raazia Razi and is{" "}
-        <a
-          href="https://github.com/Raazia-26/weather-react-App"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by Raazia Razi and is{" "}
+          <a
+            href="https://github.com/Raazia-26/weather-react-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
